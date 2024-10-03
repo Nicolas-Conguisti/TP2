2033374cabaf420e47fa0213c952a90e71502ff7
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2024-10-03 10:26:33 +0200

    Baby steaps : création de la classe Conversion avec la méthode convertir qui renvoie I

---
+++ src/main/java/com/iut/Conversion/Conversion.java
@@ -0,0 +1,9 @@
+package com.iut.Conversion;
+
+public class Conversion {
+
+    public static String convertir(int nombreArabe){
+
+        return "I";
+    }
+}
---
+++ src/test/java/com/iut/Conversion/ConversionTest.java
@@ -0,0 +1,37 @@
+package com.iut.Conversion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConversionTest {
+
+    /*
+    LISTE DES TESTS
+    - Si on converti 1 : resultat = 'I'
+    - Si on converti 2 : resultat = 'II'
+    - Si on converti 3 : resultat = 'III'
+    - Si on converti 5 : resultat = 'V'
+    - Si on converti 6 : resultat = 'VI'
+    - Si on converti 10 : resultat = 'X'
+    - Si on converti 11 : resultat = 'XI'
+    - Si on converti 4 : resultat = 'IV'
+    - Si on converti 9 : resultat = 'IX'
+    - Si on converti 49 : resultat = 'XXXXIX'
+     */
+
+    @Test
+    void convertir_1_renvoie_I() {
+
+        //GIVEN
+        int nombreArabe = 1;
+        String resultat;
+
+        //WHEN
+        resultat = Conversion.convertir(1);
+
+        //THEN
+        assertEquals("I", resultat);
+
+    }
+}
